6e34a14b3d3846cfd623df46eb056426486439f5
PRANJAL BARANWAL <77231809+Pranjal-g083@users.noreply.github.com>
2022-07-08 21:18:30 +0530

    resetting the project

--- api.js
+++
@@ -1,9 +0,0 @@
-const querystring = require('querystring');
-const { curly } = require('node-libcurl');
-
-const { statusCode, data, headers } = await curly.post('http://httpbin.com/post', {
-  postFields: querystring.stringify({
-    field: 'value',
-  }),
-  // can use `postFields` or `POSTFIELDS`
-})
